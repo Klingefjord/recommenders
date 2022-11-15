eea0b09ec4666f50dc1f8b4f857077687a923dce
Oliver Klingefjord <oliver@kry.se>
2022-11-15 22:49:59 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# MovieLens 25M
+
+This is a cross-comparison of three recommender systems trained on the [MovieLens 25M dataset](https://grouplens.org/datasets/movielens/25m/).
+
+The three models are Vanilla matrix factorization with bias, a neural network with a concatenated embedding vector, and the [Wide and Deep model](https://arxiv.org/abs/1606.07792). 
+
+The full training report can be viewed here. The neural network models performed far better than matrix factorization. 
+
+![Validation loss](./val_loss_all.png)
+
+Wide and deep performed slightly better, but the difference would probably have been bigger if more features were used in training.
+
+![Comparison between the wide and deep and neural collab models](./val_loss.png)
+
+You can use the models to find movies that you might like here.
+
--- train.py
+++ train.py
@@ -1,16 +1,12 @@
 """Train the model specified in the arguments and log results to wandb."""
 
-import torch
 import pytorch_lightning as pl
-from models.matrix_factorization import MatrixFactorizationModel
-from models.neural_collab import NeuralCollaborativeModel
-from models.wide_and_deep import WideAndDeepModel
 from loader import *
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
-import wandb
 import argparse
-import matplotlib.pyplot as plt
+
+from utils import initialize_model
 
 
 def main(
@@ -51,66 +47,6 @@ def main(
 
     # train the model
     trainer.fit(model, train_loader, validation_loader)
-
-
-def load_from_checkpoint(model_name, n_users, n_movies) -> pl.LightningModule:
-    """
-    Load a trained model from the lastest checkpoint.
-    """
-
-    if model_name == "wide-deep":
-        feature_dims = torch.tensor((n_users, n_movies))
-        hidden_dims = (64, 32)
-        embedding_dims = 50
-
-        return WideAndDeepModel.load_from_checkpoint(
-            "./checkpoints/di7rt2ln/checkpoints/epoch=19-step=1562500.ckpt",
-            feature_dims=feature_dims,
-            hidden_dims=hidden_dims,
-            embedding_dims=embedding_dims,
-            dropout=0.5,
-        )
-    elif model_name == "neural-collab":
-        return NeuralCollaborativeModel.load_from_checkpoint(
-            "./checkpoints/2faf3tez/checkpoints/epoch=19-step=1562500.ckpt",
-            n_users=n_users,
-            n_items=n_movies,
-            n_factors=50,
-            n_hidden=64,
-        )
-    elif model_name == "matrix-factorization":
-        return MatrixFactorizationModel.load_from_checkpoint(
-            "./checkpoints/1o8gw1zp/checkpoints/epoch=19-step=3125000.ckpt",
-            n_users=n_users,
-            n_items=n_movies,
-            n_factors=50,
-        )
-
-
-def initialize_model(model_name, n_users, n_movies) -> pl.LightningModule:
-    """
-    Initialize a PyTorch Lightning model.
-    """
-
-    if model_name == "wide-deep":
-        feature_dims = torch.tensor((n_users, n_movies))
-        hidden_dims = (64, 32)
-        embedding_dims = 50
-
-        return WideAndDeepModel(
-            feature_dims=feature_dims,
-            hidden_dims=hidden_dims,
-            embedding_dims=embedding_dims,
-            dropout=0.5,
-        )
-    elif model_name == "neural-collab":
-        return NeuralCollaborativeModel(
-            n_users, n_movies, num_factors=50, num_hidden=64
-        )
-    elif model_name == "matrix-factorization":
-        return MatrixFactorizationModel(n_users, n_movies, n_factors=50)
-
-    raise ValueError(f"Model '{model_name}' not supported.")
 
 
 if __name__ == "__main__":
